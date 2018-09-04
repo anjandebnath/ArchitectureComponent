e24f85c5c887600532ac862dbc29a69fa8172cb9
anjan <anjan@w3engineers.com>
2018-09-04 12:25:29 +0600

    navigation drawer

---
+++ app/src/main/java/com/anjan/architecturecomponent/ui/navigation/BackupFragment.java
@@ -0,0 +1,34 @@
+package com.anjan.architecturecomponent.ui.navigation;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anjan.architecturecomponent.R;
+
+/**
+ * Created by Anjan Debnath on 9/3/2018.
+ * Copyright (c) 2018, W3 Engineers Ltd. All rights reserved.
+ */
+public class BackupFragment extends Fragment {
+
+    public static BackupFragment newInstance(){
+        return new BackupFragment();
+    }
+
+    public BackupFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_backup, container, false);
+    }
+}
